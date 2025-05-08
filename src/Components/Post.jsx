d1c90b29b3c004c8ba9dcdbab1df429cab061040
Ahmed Lemssiah <art3que@gmail.com>
2025-05-08 09:22:23 +0200

    Learning

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -1,7 +1,12 @@
+const names = ['Maximilian', 'Manuel']
+
 const Post = () => {
+
+  const choosenName = Math.random() > 0.5  ? names[0] : names[1]
+
   return (
     <div>
-      <p>Maximilia</p>
+      <p>{choosenName}</p>
       <p>React.js is awesome!</p>
     </div>
   );
